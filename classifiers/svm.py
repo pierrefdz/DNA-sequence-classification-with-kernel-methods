3e4af34c46d1d817b2dcbab8ee5ea7517752ab62
Paul Jacob <43207335+Poljy@users.noreply.github.com>
2021-03-14 16:34:14 +0100

    Add SVM

--- classifiers/svm.py
+++ classifiers/svm.py
@@ -1,5 +1,7 @@
 import numpy as np
 import cvxopt
+from cvxopt import matrix
+import cvxpy as cp
 
 class SVM():
     """
@@ -11,13 +13,72 @@ class SVM():
         svm.predict(X_test)
     """
 
-    def __init__(self, kernel, C):
+    def __init__(self, kernel, C=1.0):
+        """
+        C: float > 0, default=1.0, regularization parameter
+        """
         self.kernel = kernel
         self.C = C
-        pass
 
     def fit(self, X, y):
-        pass
+
+        self.X_train = X
+        n_samples = X.shape[0]
+        self.X_train_gram = self.kernel.gram(X)
+
+        #Define the optimization problem to solve
+        P = (y @ y.T) * self.X_train_gram
+        q = -np.ones(n_samples)
+        G = np.block([[np.eye(n_samples)],[-np.eye(n_samples)]])
+        h = np.concatenate((self.C*np.ones(n_samples),np.zeros(n_samples)))
+
+        #Solve the problem
+        
+        #With cvxopt
+        P=matrix(P)
+        q=matrix(q)
+        G=matrix(G)
+        h=matrix(h)
+        solver = cvxopt.solvers.qp(P=P,q=q,G=G,h=h)
+        x = solver['x']
+        self.alphas = np.squeeze(y)*np.squeeze(np.array(x))
+        
+        """
+        #With cvxpy
+        x = cp.Variable(n_samples)
+        objective = cp.Minimize((cp.quad_form(x, P) + 2 * q.T @ x))
+        constraints = [G @ x <= h]
+        prob = cp.Problem(objective,constraints)
+        prob.solve()
+        print(x.value)
+        self.alphas = np.squeeze(y)*np.array(x.value)
+        """
+
+        #Retrieve the support vectors
+        self.support_vectors_indices = np.squeeze(np.array(x)) > 1e-4
+        self.alphas = self.alphas[self.support_vectors_indices]
+        self.support_vectors = self.X_train[self.support_vectors_indices]
+
+        print(len(self.support_vectors), "support vectors out of",len(self.X_train), "training samples")
+
+        return self.alphas
+
 
     def predict(self, X):
-        pass
+        """
+        X: array (n_samples, n_features)\\
+        Return: float array (n_samples,)
+        """
+        K = self.kernel.gram(X, self.support_vectors)
+        y = np.dot(K, self.alphas)
+        return y
+
+    def predict_classes(self, X, threshold=0):
+        """
+        X: array (n_samples, n_features)\\
+        Return: 0 and 1 array (n_samples,)
+        """
+        K = self.kernel.gram(X, self.support_vectors)
+        y = np.dot(K, self.alphas)
+        return np.where(y > threshold, 1, -1)
+
--- sandbox.py
+++ sandbox.py
@@ -6,6 +6,7 @@ Sandbox file to try things messily
 import numpy as np
 import pandas as pd
 from sklearn.kernel_ridge import KernelRidge
+from sklearn.svm import SVC
 
 from kernels import LinearKernel, GaussianKernel, PolynomialKernel
 
@@ -39,6 +40,8 @@ Y0_train = pd.read_csv("data/Ytr0.csv", sep=",", index_col=0).values
 Y1_train = pd.read_csv("data/Ytr1.csv", sep=",", index_col=0).values
 Y2_train = pd.read_csv("data/Ytr2.csv", sep=",", index_col=0).values
 
+
+"""
 # %%
 kernel = LinearKernel()
 ridge = RidgeRegression(kernel=kernel, alpha=0.01)
@@ -56,3 +59,36 @@ ridge.predict(X0_mat100_train)
 # Y0_train_scaled = np.where(Y0_train == 0, -1, 1)
 # logreg.fit(X0_mat100_train, Y0_train)
 # logreg.predict(X0_mat100_train)
+
+"""
+
+Y0_train_scaled = np.where(Y0_train == 0, -1, 1)
+
+print("Real classes distribution:")
+
+print(len(Y0_train_scaled), "samples")
+print(np.sum(Y0_train_scaled== 1), "positive")
+print(np.sum(Y0_train_scaled==-1), "negative")
+
+compare_svms = True
+
+if compare_svms:
+
+    #Sklearn SVM
+    svm_scikit = SVC(C=100.0,kernel='linear')
+    svm_scikit.fit(X0_mat100_train, Y0_train_scaled)
+    svm_scikit_classes = svm_scikit.predict(X0_mat100_train)
+
+    #My SVM
+    my_svm = SVM(kernel=LinearKernel(),C=100.0)
+    my_svm.fit(X0_mat100_train, Y0_train_scaled)
+
+    y_pred = my_svm.predict(X0_mat100_train)
+    y_classes_pred = my_svm.predict_classes(X0_mat100_train)
+
+
+    print("Accuracy on train (sklearn SVM):", np.sum(svm_scikit_classes==np.squeeze(Y0_train_scaled))/len(Y0_train_scaled))
+    print("Accuracy on train (my SVM):", np.sum(np.squeeze(y_classes_pred)==np.squeeze(Y0_train_scaled))/len(Y0_train_scaled))
+    print("Similarity between sklearn SVM and my SVM:",np.sum(np.squeeze(y_classes_pred)==np.squeeze(svm_scikit_classes))/len(Y0_train_scaled))
+
+
